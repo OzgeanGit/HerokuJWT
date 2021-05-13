4bb8d9408cb031b2129f07391f5f08f6c08adff0
ozgeangit <ozgeanmitrof@gmail.com>
2021-05-13 23:21:35 +0300

    csrf + postgresql + mapping

--- src/main/java/login/microservice/JWT/Spring/Security/config/SecurityConfig.java
+++ src/main/java/login/microservice/JWT/Spring/Security/config/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .httpBasic().disable()
+                .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
