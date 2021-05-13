124b9016728d00405eb249ad60252b5ada1d98b1
ozgeangit <ozgeanmitrof@gmail.com>
2021-05-13 23:14:44 +0300

    csrf + postgresql + mapping

--- src/main/java/login/microservice/JWT/Spring/Security/controller/TestSecurityController.java
+++ src/main/java/login/microservice/JWT/Spring/Security/controller/TestSecurityController.java
@@ -1,22 +1,24 @@
 package login.microservice.JWT.Spring.Security.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path="/permission")
 public class TestSecurityController {
 
-    @GetMapping("/admin/get")
+    @GetMapping("/admin")
     public String getAdmin() {
         return "Hi admin!";
     }
 
-    @GetMapping("/student/get")
+    @GetMapping("/student")
     public String getStudent() {
         return "Hi student!";
     }
 
-    @GetMapping("/professor/get")
+    @GetMapping("/professor")
     public String getProfessor() {
         return "Hi Professor!";
     }
