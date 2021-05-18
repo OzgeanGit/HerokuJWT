aec15bf6d2f700d01f5f8518d06d6562055da96c
ozgeangit <ozgeanmitrof@gmail.com>
2021-05-18 09:54:24 +0300

    +update role

---
+++ src/main/java/login/microservice/JWT/Spring/Security/controller/UpdateRoleRequest.java
@@ -0,0 +1,11 @@
+package login.microservice.JWT.Spring.Security.controller;
+import lombok.Data;
+    @Data
+    public class UpdateRoleRequest {
+
+        private String login;
+
+        private String role;
+
+
+    }
